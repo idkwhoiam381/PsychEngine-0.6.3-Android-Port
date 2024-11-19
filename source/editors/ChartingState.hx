941cb9ea8bcb08a1126333000a75ca1aacc07f22
Prohack101010 <miracberaber@gmail.com>
2024-11-19 17:51:36 +0300

    Okay

--- source/editors/ChartingState.hx
+++ source/editors/ChartingState.hx
@@ -3068,7 +3068,7 @@ class ChartingState extends MusicBeatState
 		if ((data != null) && (data.length > 0))
 		{
 			#if mobile
-			StorageUtil.saveContent(Paths.formatToSongPath(_song.song) + postfix, ".json", data.trim());
+			StorageUtil.saveContent(Paths.formatToSongPath(_song.song) + postfix + ".json", data.trim());
 			#else
 			_file = new FileReference();
 			_file.addEventListener(Event.COMPLETE, onSaveComplete);
@@ -3099,7 +3099,7 @@ class ChartingState extends MusicBeatState
 		if ((data != null) && (data.length > 0))
 		{
 			#if mobile
-			StorageUtil.saveContent("events", ".json", data.trim());
+			StorageUtil.saveContent("events.json", data.trim());
 			#else
 			_file = new FileReference();
 			_file.addEventListener(Event.COMPLETE, onSaveComplete);
